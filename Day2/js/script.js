a2f83a0c397f62b8575a51f87cc029e1c16609be
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-09 19:18:19 -0400

    Created Starting files for Day 2
    Created files in webstorm

---
+++ Day2/js/script.js
@@ -0,0 +1,21 @@
+/*
+This is going to be  multi lined comment.
+Brandyn Stroud
+7/9/2015
+SDI Section 00
+Day 2 Coding Basics
+ */
+
+//alert("This is making sure our JS file is completed.");
+
+//This is a single lined comment
+//usually describe what comes after it
+
+/*
+Multi lined comment
+Anything between the star and slash is ignored by the browser
+ */
+
+//Alert- gives user information but returns nothing to us
+
+alert("Welcome to SDI");
