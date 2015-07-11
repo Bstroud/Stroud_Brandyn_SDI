554ec7d69b854a32dd0302f65fbc9e0a0282660c
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-11 18:03:08 -0400

    Items in array

--- Day 3/js/day3_arrays.js
+++ Day 3/js/day3_arrays.js
@@ -6,3 +6,19 @@ Arrays
  */
 
 //alert("It works");
+
+//create a basic array
+//Picking oranges
+var orangeBins = [30, 500, 10002];
+
+//How many oranges did we pick
+var totalOranges = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(totalOranges+ " oranges were picked in 3 days.");
+
+
+//Length properties of arrays
+console.log(orangeBins.length);
+
+
+var averageOranges = totalOranges /orangeBins.length;
+console.log("The average number of oranges is " +averageOranges + ".");
