b103003cab953053c75d3de1fa3ee782081a6d46
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-14 19:52:13 -0400

    Else if statements

--- Day 4/js/da4_conditionals.js
+++ Day 4/js/da4_conditionals.js
@@ -30,14 +30,19 @@ if(oldEnough){
 
 //Test the kids height to see if he can ride
 
-var kidHeight = 42;
+var kidHeight = prompt("How tall are you in inches?");
 
-var sneakerLength = 2
+var sneakerLength = 2;
+var minHeight = 48;
 
 //if the childs height is 48 or taller they can ride the ride
 if(kidHeight>=48){
     //Code will run if child is 48 inches or taller
     console.log("Congrats! You can ride!");
+} else if(Number(kidHeight+sneakerLength >=minHeight)){
+    // This code will run if the child is tall enough will wearing sneakers
+    console.log("You are tall enough to ride this ride if you wear shoes!\nCheater!");
+
 } else {
     console.log("sorry too short.")
 }
