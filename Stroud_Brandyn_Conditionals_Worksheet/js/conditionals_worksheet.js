61f93e79b12a3349164fee73b884d81de53caa0d
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-14 22:26:50 -0400

    Brainstorming group 3

---
+++ Stroud_Brandyn_Conditionals_Worksheet/js/conditionals_worksheet.js
@@ -0,0 +1,20 @@
+/**
+ Brandyn Stroud
+ Section 00
+ 7-14-2015
+ Conditionals Worksheet homework
+ */
+
+//alert("It works...");
+
+//MOVIE TICKET PRICE
+
+var regPrice = 12.00;
+var discountPrice = 7.00;
+var qualify = true;
+alert("Time:\n2:00");
+prompt("Do you qualify for or Senior/Child discount offer?");
+if(qualify===true){
+    prompt("Please enter your age:");
+}
+console.log("Congrats you qualify for our discount offer.");
