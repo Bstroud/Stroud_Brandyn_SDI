e8b0810cdd6a1f6ed72453548c6db5e2a8bf2421
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-14 22:43:06 -0400

    Group 3 redo

--- Stroud_Brandyn_Conditionals_Worksheet/js/conditionals_worksheet.js
+++ Stroud_Brandyn_Conditionals_Worksheet/js/conditionals_worksheet.js
@@ -10,11 +10,13 @@
 //MOVIE TICKET PRICE
 
 var regPrice = 12.00;
-var discountPrice = 7.00;
-var qualify = true;
+var discountPrice = 55;
+var discountPrice2 = 10;
 alert("Time:\n2:00");
-prompt("Do you qualify for or Senior/Child discount offer?");
-if(qualify===true){
-    prompt("Please enter your age:");
+prompt("Please enter your age:");
+if(discountPrice>=55 || discountPrice2<=10){
+    console.log("Congrats you qualify for our discount offer!!!");
+
+} else {
+    console.log("You don't qualify for our discount offer please pay " +regPrice);
 }
-console.log("Congrats you qualify for our discount offer.");
