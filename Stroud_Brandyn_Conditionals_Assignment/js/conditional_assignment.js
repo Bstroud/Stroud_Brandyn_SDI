c2f10679c46561889e1b59273985a5425ee9dcce
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-16 22:52:10 -0400

    Condtionals Assignment 1

---
+++ Stroud_Brandyn_Conditionals_Assignment/js/conditional_assignment.js
@@ -0,0 +1,26 @@
+/*
+Brandyn Stroud
+Section 00
+7-16-2015
+Conditional Assignment
+ */
+
+
+//alert("It works");
+
+//Assuming you live 1 hour away from school, work, etc., what time should you leave to make sure you arrive before
+//9:00 in the morning. This is assuming you are driving and live in a moderately populated area like Orlando.
+
+var alarm = 6;
+var showerAndDressed = 35;
+var traffic = 45;
+
+//If you wake up on time at 6 am
+
+console.log("Try to make it before 9am!");
+prompt("At what time do you wake up in the morning?\nEnter the hour closest to this time");
+prompt("How long does it take you to shower?\nEnter the amount of mintutes it takes you.");
+alert("No matter what it will take you 45 minutes in traffic regardless of how fast you drive");
+
+
+
