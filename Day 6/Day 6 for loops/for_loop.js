3b9dcf3abb75843d18562f38253d0d2ad1efad22
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-18 20:37:52 -0400

    for loops

---
+++ Day 6/Day 6 for loops/for_loop.js
@@ -0,0 +1,25 @@
+/*Brandyn Stroud
+ Section 00
+ 7-18-2015
+ For loops
+ */
+
+//alert("More loops");
+
+//Basic for loop
+//for(initilize counter: condition; increment of change){}
+
+for(var i = 10; i>0; i--){
+    console.log("The value of i is " +i);
+}
+
+
+//breaks - if this command is run your loop will stop
+for(var j = -10; j<5; j--){
+    console.log("the value of j is " +j);
+
+    //If the value is of j is 3 stop my loop
+    if(j===3){
+        break
+    }
+}
