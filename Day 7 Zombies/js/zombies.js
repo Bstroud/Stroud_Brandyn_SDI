afbb8b359154560e2ae9217885da683075e077ef
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-21 19:02:04 -0400

    while loop for zombies

--- Day 7 Zombies/js/zombies.js
+++ Day 7 Zombies/js/zombies.js
@@ -3,7 +3,7 @@
  Section 00
  7-21-2015
  Zombies
- */
+*/
 
 //alert("ZOMBIEEESSS");
 
@@ -20,7 +20,7 @@ var zomBites = 4;
 
 //What is our timeline
 var days = 8;
-
+/*
 for(var i = 1; i<=days; i++){
 
  //How many people get infected everyday
@@ -31,3 +31,26 @@ for(var i = 1; i<=days; i++){
  console.log("On day number " + i + " there will be " + zombies + " zombies!");
 
 }
+*/
+
+//The CDC wants to know how many days it will take for their to be a million zombies
+
+//Variable to count the number of days
+var numdays = 1;
+
+while(zombies<=10000000){
+
+ //How many people get infected everyday
+ var infectedPeople = zombies * zomBites;
+
+ //How many zombies will we have?
+ zombies += zombies + infectedPeople;
+
+ //report out each day
+ console.log("On day number " + numdays + " there will be " + zombies + " zombies!");
+
+ //increment the days
+ numdays++
+
+}
+console.log("It will take " + (numdays-1) + " days to make a million zombies.");
