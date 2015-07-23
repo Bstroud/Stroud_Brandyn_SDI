e9a0e72167e3f9c68288a566c87b3c06445c0c88
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-23 18:41:48 -0400

    array loops again

--- Stroud_Brandyn_Arrayloop/js/arrayloop.js
+++ Stroud_Brandyn_Arrayloop/js/arrayloop.js
@@ -12,18 +12,20 @@
     //The turtles
 var turtles = ["Leonardo", "Raphael", "Donatello", "Michelangelo"];
 
-//Who is your favorite of the 4
+//Who is your favorite Turtle
 var favTurtle = prompt("Who is your favorite turtle?");
+
 console.log(favTurtle);
-while(turtles.toString() != turtles){
- favTurtle = prompt("Please enter a turtle name.\nLeonardo, Raphael, Donatello, Michelangelo.");
- counter++;
- if(counter > 3){
-  console.log("You must like The Shredder.")
- }
+
+
+
+//Validation
+favTurtle = turtles.length;
+if(favTurtle = turtles.length){
+
+console.log("Cowabunga!");
+
+ }else if(turtles != turtles.length){
+ prompt("Please enter a turtle name.");
 }
 
-for(var i = 0; i < turtles.length; i++){
-
- console.log(turtles[i])
-}
