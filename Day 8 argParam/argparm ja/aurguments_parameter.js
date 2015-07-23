c6ebfd446dccdf4ce1b291068753371e346c0a3f
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-23 19:25:15 -0400

    put info into our functions

--- Day 8 argParam/argparm ja/aurguments_parameter.js
+++ Day 8 argParam/argparm ja/aurguments_parameter.js
@@ -12,20 +12,54 @@
 
 //Create variables for width and height
 
-
+var width =10;
+var height = 20;
 //Create a function to calculate the area of a rectangle
-function calcArea() {
+function calcArea(w, h) {
 
 
 //create var
- var width = 10;
- var height = 20;
+ //var width = 10;
+ //var height = 20;
 
 //calculate the area
 
- var area = width * height;
+ var area = w * h;
 
 //console.log
- console.log("The area of a rectangle with a width of " + width + " and a height of " + height + " is " + area);
+ console.log("The area of a rectangle with a width of " + w + " and a height of " + h + " is " + area);
 }
-calcArea();
+calcArea(width, height);
+
+//Find the area, width = 6 and height = 7
+calcArea(6,7);
+
+//Ask the user
+var userWidth = prompt("Lets calculate the area of a rectangle\nPlease enter a width.");
+
+//Validate and number cast the value
+var userHeight = prompt("Please enter a height.");
+calcArea(userHeight, userHeight);
+
+
+//Create a function that will calculate dog years
+function dogYears(humanAge){
+ //input a human year and spit out a dog yeat
+ //humanAge*7 = dogAge
+ var dogAge = humanAge*7;
+
+ //Print out results
+ console.log("A dog who is " +humanAge+ " is " +dogAge+ " in dog years.");
+
+}
+dogYears();
+
+//Calculate how old a 4 year old dog is in dog years
+dogYears(7);
+
+//Prompt the user
+var yourDogAge = prompt("Let' figure out the age of your dog is in dog years.\nHow old is your dog?");
+
+//Validate and cast if needed
+
+dogYears(yourDogAge);
