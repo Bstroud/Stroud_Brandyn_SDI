7b68c42a96e3cabbe5eb08477b2b278a512bd631
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-23 19:01:08 -0400

    argument1

--- Day 8 argParam/argparm ja/aurguments_parameter.js
+++ Day 8 argParam/argparm ja/aurguments_parameter.js
@@ -5,4 +5,27 @@
  Arguments and Parameter
  */
 
-alert("IT works");
+//alert("IT works");
+
+//Argumets- goes into the function call - "specific"
+//Parameters - goes into the function definition and these catch the arguments = "Generic containers"
+
+//Create variables for width and height
+
+
+//Create a function to calculate the area of a rectangle
+function calcArea() {
+
+
+//create var
+ var width = 10;
+ var height = 20;
+
+//calculate the area
+
+ var area = width * height;
+
+//console.log
+ console.log("The area of a rectangle with a width of " + width + " and a height of " + height + " is " + area);
+}
+calcArea();
