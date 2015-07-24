9c2bdc665e793f598cc1ef1ca4d28cca6941e11a
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-23 21:43:28 -0400

    Functions Worksheet 1

---
+++ Stroud_Brandyn_Function_Worksheet/js/function_worksheet.js
@@ -0,0 +1,14 @@
+/**
+ Brandyn Stroud
+ Section 00
+ 7-23-2015
+ Function Worksheet
+ */
+
+//Variables for formula of circumference
+var pi = 3.14;
+var radius = prompt("Choose a number to multiply by pi");
+var circumference = pi*Math.pow(radius, 2);
+
+console.log(circumference);
+
