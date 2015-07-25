4b12bf4bba19f0e8546689ed02d77a91ca253f5b
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-25 17:37:58 -0400

    prompted and validated min value

--- Day 9/js/random.js
+++ Day 9/js/random.js
@@ -7,3 +7,19 @@
 
 //alert("It works");
 
+//Create a function that takes a user max a nd min value and return a random number inbetween
+
+//Create a variable for the main variable
+var min = prompt("We are going to find a random nimber between to random numbers\nWhat is the minimum value.");
+//validate prompt
+while(isNaN(min || min==="")){
+ if(isNaN(min)){
+  min=prompt("Please only type in numbers. What is the minimum value");
+
+ }else{
+  min=prompt("Please don not leave blank.\nPlease enter a minimum value.");
+ }
+
+}
+//Number cast or parseInt
+min=Number(min);
