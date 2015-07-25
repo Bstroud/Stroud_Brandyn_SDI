9cfb1f94341b6924191af7a0ea64a34eda35c07f
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-25 19:00:50 -0400

    procedures
    they don’t return

---
+++ Day 9 Procedures/js/procedures.js
@@ -0,0 +1,35 @@
+/**
+ Brandyn Stroud
+ Section 00
+ 7-25-2015
+ PROCEDURES
+ */
+
+
+//alert("It works");
+
+//This is a normal function
+function calcArea(w, h){
+ var area = w*h;
+
+ //returns a variable
+ return area
+}
+
+//function call
+
+//catch the return value
+var results = calcArea(10, 20);
+
+//console.log
+console.log(results);
+
+//This is a procedure
+//A list of steps to do
+//returns nothing
+
+function calcPeri(w, h){
+  var peri = 2*w + 2*h;
+ console.log(peri)
+}
+calcPeri(10, 20);
