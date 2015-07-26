eb75dde48c931a369a6df40765d4a64da54b34c9
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-26 01:03:21 -0400

    Functions Assignment
    Working on the prompts

--- Stroud_Brandyn_Function_Assignment/function_assignment.js
+++ Stroud_Brandyn_Function_Assignment/function_assignment.js
@@ -10,8 +10,30 @@
 
 //Pokemon
 
+//Prompt the user
+    var guess = parseInt(prompt("Guess how many pokemon there were in total after the third generation.\nType in a number."));
+
+//Validation
+while(isNaN(guess) || guess==="") {
+ //Re-prompt the user
+ guess = prompt("Please only use numbers and don't leave blank.\nPlease enter a number.");
+}
+if(guess===788){
+ console.log("You're right you must be a real fan");
+}else{
+ console.log("Sorry guess again");
+}
+
 //results
 var results = pokemon();
+
+//Main Code var
+var poke1 = 151;
+var poke2 = 251;
+var poke3 = 386;
+
+var gen3 = results;
+console.log(gen3);
 
 //Function
 function pokemon(s1, s2, s3){
@@ -21,11 +43,10 @@ s1 = 151;
 
 var total = s1+s2+s3;
 
- console.log(total);
-
- return total
+return total
 
 }
 //Call function
-pokemon();
+pokemon(poke1, poke2, poke3);
+
 console.log("After 3 generations of Pokemon the total numbers of Pokemon were " +results);
