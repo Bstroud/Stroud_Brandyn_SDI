7611ba8b4bffe8567668b8f08d2bf3e2c29cb902
brandynstroud1 <b2stroud@fullsail.edu>
2015-07-25 21:50:57 -0400

    Function Assignment 1
    Starting out

---
+++ Stroud_Brandyn_Function_Assignment/function_assignment.js
@@ -0,0 +1,27 @@
+/**
+ Brandyn Stroud
+ Section 00
+ 7-25-2015
+ Function Assignment
+ */
+
+
+//alert("It works");
+
+//Counting to 100
+
+//Global variables
+var startNum = 0;
+var endNum = 100;
+
+//Function
+function aHundred(start, end){
+
+ 
+ console.log()
+
+
+}
+//Call function
+var centi = aHundred();
+console.log(centi);
